43d2aaaf736fc87ae7cd85724c858b82c8b1c6a2
zidoopal <yuzzzin@gmail.com>
2023-09-21 13:42:07 +0900

    Feat: GitHub 로그인 - [...nextauth] 파일 생성
    📁src > 📁pages > 📁api > 📁auth > [...nextauth].ts 생성

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth from 'next-auth';
+import { NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+// import KakaoProvider from '...'; // KakaoProvider가 필요하다면 해당 경로를 추가하세요.
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: 'ed7cac4b32aadeae559e',
+      clientSecret: 'd92c1b933e89f89fab558cd5c4bac08b177111e1',
+    }),
+    // KakaoProvider({
+    //   clientId: 'ed7cac4b32aadeae559e',
+    //   clientSecret: 'd92c1b933e89f89fab558cd5c4bac08b177111e1',
+    // }),
+  ],
+  secret: 'qwerty1234',
+};
+
+export default NextAuth(authOptions);
+export { authOptions };
