cba60e69cb128af3e4d2368aca86f57d882627bb
zidoopal <yuzzzin@gmail.com>
2023-09-22 14:00:02 +0900

    Fix: prop 수정 (++type 추가)

--- src/components/Input/LoginInput.tsx
+++ src/components/Input/LoginInput.tsx
@@ -7,10 +7,11 @@ import TextField from '@mui/material/TextField';
 interface LoginInputProps {
   color: 'primary' | 'neutral' | 'danger' | 'success' | 'warning';
   placeholder?: string;
-  type?: 'text' | 'password';
+  type?: 'text' | 'password' | 'email'; // 'email' 추가
   icon?: React.ReactNode;
   onIconClick?: () => void;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
 }
 
 const LoginInput: React.FC<LoginInputProps> = ({
@@ -20,14 +21,15 @@ const LoginInput: React.FC<LoginInputProps> = ({
   icon,
   onIconClick,
   onChange,
+  value,
 }) => {
   return (
     <TextField
       type={type}
       placeholder={placeholder}
       variant="outlined"
-      color={color as any} // Color type might differ, so casting as any for now
-      fullWidth // TextField usually requires this for proper styling
+      color={color as any}
+      fullWidth
       sx={{ marginBottom: '10px', padding: '8px' }}
       InputProps={{
         endAdornment: icon && (
@@ -37,6 +39,7 @@ const LoginInput: React.FC<LoginInputProps> = ({
         ),
       }}
       onChange={onChange}
+      value={value}
     />
   );
 };
