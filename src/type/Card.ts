57f393abedf84b2d133771e483167249d1cc68fb
zidoopal <yuzzzin@gmail.com>
2023-09-25 04:42:45 +0900

    Update Card.ts

--- src/type/Card.ts
+++ src/type/Card.ts
@@ -1,10 +1,40 @@
-export type Card = {
-    id:number;
-    writer:string;
-    title: string;
-    content: string;
-    image: string;
-    views: string | number;
-    likes: string | number;
-    category: string;
-}
+export type Tester = {
+  id: number;
+  userId: string;
+  title: string;
+  content: string;
+  image: string;
+  views: string | number;
+  likes: string | number;
+  category: string;
+};
+
+export type Questions = {
+  id: number;
+  title: string;
+  image: string;
+  testId: number;
+  choices: Choice[];
+};
+
+export type Choice = {
+  id: number;
+  questionId: number;
+  content: string;
+  isCorrect: boolean;
+};
+
+export type Comment = {
+  id: number;
+  content: string;
+  userId: number;
+  testId: number;
+};
+
+export type User = {
+  id: number;
+  userId: string;
+  password: string;
+  nickname: string;
+  image: string;
+};
