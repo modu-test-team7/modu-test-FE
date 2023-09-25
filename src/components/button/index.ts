65fe2bafe24a5d334c9ae9750971919821105682
zidoopal <yuzzzin@gmail.com>
2023-09-26 07:57:55 +0900

    Update index.ts

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,6 +1,7 @@
 import Button from './Button';
 import ButtonGroup from './ButtonGroup';
 import OAuthButton from './OAuthButton';
+import MypageButton from './MypageButton';
 import UpButton from './UpButton';
 
-export { Button, ButtonGroup, OAuthButton, UpButton }
+export { Button, ButtonGroup, OAuthButton, UpButton, MypageButton };
