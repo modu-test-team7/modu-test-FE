d20e3deb5c8ad3cd122134e3226cdcae6ffc0cc0
zidoopal <yuzzzin@gmail.com>
2023-09-26 15:51:23 +0900

    Update MypageButton.tsx

--- src/components/button/MypageButton.tsx
+++ src/components/button/MypageButton.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
 interface MypageButtonProps {
-  label: string;
+  type: 'button';
   onClick?: () => void;
-}
+  className?: string;
+  label: string;
 
-const MypageButton: React.FC<MypageButtonProps> = ({ label, onClick }) => {
+const MypageButton: React.FC<MypageButtonProps> = ({
+  label,
+  type = 'button',
+  onClick,
+  className,
+}) => {
+  const buttonStyle = `
+    w-32 ml-5 p-2 rounded-xl transition duration-300 ease-in-out hover:bg-[#11B767] z-30 bg-[#ffffff]
+  `;
   return (
-    <button
-      onClick={onClick}
-      className="w-32 ml-5 p-2 rounded-xl transition duration-300 ease-in-out hover:bg-[#11B767] z-30 bg-[#ffffff]"
-    >
+    <button onClick={onClick} className={buttonStyle}>
       {label}
     </button>
   );
