f52559182a9147c750e9db39f8679ef18478153c
zidoopal <yuzzzin@gmail.com>
2023-09-27 17:01:50 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: ['example.com'],
+    unoptimized: true,
   },
 };
 
-module.exports = {
-  images: {
-    domains: ['example.com'], // 외부 이미지 도메인을 여기에 추가하세요
-  },
-};
+module.exports = nextConfig;
