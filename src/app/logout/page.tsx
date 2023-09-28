b7436270165d3ea22b75c42d4deb7d28d331756b
zidoopal <yuzzzin@gmail.com>
2023-09-28 13:48:01 +0900

    수정수정

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { postAPI } from '@/config/axios';
 import Cookies from 'js-cookie';
 
